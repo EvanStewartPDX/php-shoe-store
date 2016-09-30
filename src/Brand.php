0887069f577356ad0634c7cb8ad500576152151b
Evan <stewarea@gmail.com>
2016-09-30 09:12:19 -0700

    add save and getall functions to brand

--- src/Brand.php
+++ src/Brand.php
@@ -3,9 +3,9 @@
     {
 
         private $name;
-        private $id = null;
+        private $id;
 
-        function __construct($name, $id)
+        function __construct($name, $id = null)
         {
             $this->name = $name;
             $this->id = $id;
@@ -22,6 +22,28 @@
         function getId()
         {
             return $this->id;
+        }
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brand (name) VALUES ('{$this->getName()}')");
+            $this->id= $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brand;");
+            $brands = array();
+            foreach($returned_brands as $brand) {
+                $id = $brand['id'];
+                $name = $brand['name'];
+                $new_brand = new Brand($name, $id);
+                array_push($brands, $new_brand);
+            }
+            return $brands;
+        }
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM brand;");
         }
 
     }
--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -14,15 +14,15 @@
 
     class BrandTest extends PHPUnit_Framework_TestCase
     {
-      // protected function tearDown()
-      //   {
-      //     Brand::deleteAll();
-      //   }
+      protected function tearDown()
+        {
+          Brand::deleteAll();
+        }
         function test_getName()
         {
             //Arrange
             $id = null;
-            $name = "History";
+            $name = "Keds";
 
             $test_Brand = new Brand($name, $id);
 
@@ -32,6 +32,55 @@
             //Assert
             $this->assertEquals($name, $result);
         }
+        function test_getId()
+        {
+            $id = null;
+            $name = "Keds";
+
+            $test_Brand = new Brand($name, $id);
+
+            //Act
+            $result = $test_Brand->getId();
+
+            //Assert
+            $this->assertEquals($id, $result);
+
+        }
+        function test_save()
+        {
+            //Arrange
+            $name = "Keds";
+
+            $test_Brand = new Brand($name);
+            $test_Brand->save();
+
+            //Act
+            $result = Brand::getAll();
+
+            //Assert
+            $this->assertEquals($test_Brand, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $id = null;
+            $name = "Keds";
+            $test_Brand = new Brand($name, $id);
+            $test_Brand->save();
+
+            $id1 = null;
+            $name1 = "Red Wing";
+            $test_Brand1 = new Brand($name, $id);
+            $test_Brand1->save();
+            //Act
+            $result = Brand::getAll();
+
+            //Assert
+            $this->assertEquals([$test_Brand, $test_Brand1], $result);
+
+        }
+
     }
 
   ?>
